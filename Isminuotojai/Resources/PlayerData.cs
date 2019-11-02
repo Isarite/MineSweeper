4bd70502b1c6c22559d11c0ded4394fa75824cc8
Unknown <rujsamod@gmail.com>
2019-11-02 15:10:37 +0200

    Create User on client

--- Isminuotojai/Resources/PlayerData.cs
+++ Isminuotojai/Resources/PlayerData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,9 @@ namespace Isminuotojai.Resources
 {
     public class PlayerData
     {
+        [JsonProperty("userName")]
         public string userName;
+        [JsonProperty("password")]
         public string password;
     }
 }
