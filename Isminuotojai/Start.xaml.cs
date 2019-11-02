683ba4e4ab1d2a8b869ea45646c7145b3fb50b7c
Hitasss <vytisjons@gmail.com>
2019-11-02 14:53:15 +0200

    startScreen

---
+++ Isminuotojai/Start.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Isminuotojai
+{
+    /// <summary>
+    /// Interaction logic for Start.xaml
+    /// </summary>
+    public partial class Start : Window
+    {
+        public Start()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_register_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
